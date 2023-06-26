4b2da6044781ff7cca85f45ce27a9c7e84946219
Frankie <frankie@episodemaker.com>
2023-06-26 15:26:34 -0400

    add ga

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Skills from "./components/Skills";
 import Work from "./components/Work";
+import ReactGA from "react-ga";
 
+const TRACKING_ID = "G-320HLE1QWS"; // OUR_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
 function App() {
   return (
     <div>
